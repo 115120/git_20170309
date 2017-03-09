b4ffe1fe6302ec6e47e9f007d355693de886aae9
115120 <1156438120@qq.com>
2017-03-09 14:46:41 +0800

    第二次提交

--- app/src/main/java/com/example/administrator/videonews/activity/MainActivity.java
+++ app/src/main/java/com/example/administrator/videonews/activity/MainActivity.java
@@ -1,16 +1,24 @@
 package com.example.administrator.videonews.activity;
 
-import android.support.v7.app.AppCompatActivity;
+
 import android.os.Bundle;
+import android.widget.Button;
 
 import com.example.administrator.videonews.R;
+import com.example.administrator.videonews.base.MyBaseActivity;
 
-public class MainActivity extends AppCompatActivity {
+import butterknife.BindView;
+import butterknife.ButterKnife;
 
+public class MainActivity extends MyBaseActivity {
+    @BindView(R.id.mai_but) Button mai_but;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        //绑定到Activity上
+        ButterKnife.bind(this);
+        mai_but.setText("大傻逼");
 
     }
 
